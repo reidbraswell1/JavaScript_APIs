0b405f5807b9a262793feaf852cdd321dcbd9012
Reid Braswell <reidbras@gmail.com>
2021-09-11 17:00:25 -0500

    Adding function to blank images also removed dummy test of getGiph function

--- app.js
+++ app.js
@@ -3,21 +3,66 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function blankImages() {
+    img1.src = "#";
+    img2.src = "#";
+    img3.src = "#";
+    img4.src = "#";
+    img5.src = "#";
+}
+function controller(inputSearch) {
+  console.log("---Begin controller()---");
+  console.log(`Parameters=${inputSearch}`);
+  getGiph(inputSearch)
+    .then(function (response, reject) {
+      console.log(
+        "Response returned to getGiph = " + JSON.stringify(response, null, 2)
+      );
+      blankImages();
+      for (let i = 0; i < response.length; i++) {
+        console.log(`urls=${response[i].images.original.url}`);
+        console.log(response[i].images.original.url);
+        switch (i) {
+          case 0:
+            img1.src = response[i].images.original.url;
+            break;
+          case 1:
+            img2.src = response[i].images.original.url;
+            break;
+          case 2:
+            img3.src = response[i].images.original.url;
+            break;
+          case 3:
+            img4.src = response[i].images.original.url;
+            break;
+          case 4:
+            img5.src = response[i].images.original.url;
+            break;
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log(`An Error has occurred = ${error}`);
+    });
+    document.getElementById("exercise-form").reset();
+}
+
 async function getGiph(searchKey) {
-    console.log("---Begin getGiph---");
+  console.log("---Begin getGiph()---");
+  console.log(`Parameters=${searchKey}`)
 
-    const baseURL = "https://api.giphy.com/v1/gifs/search?";
-    const apiKey = "api_key=mUiZ6FGejMTommITwThDmUr016iX5GAG";
-    const query = "&q=";
-    const limitOffsetRatingLang = "&limit=5&offset=0&rating=g&lang=en";
-    let fetchURL = `${baseURL}${apiKey}${query}${searchKey}${limitOffsetRatingLang}`;
+  const baseURL = "https://api.giphy.com/v1/gifs/search?";
+  const apiKey = "api_key=mUiZ6FGejMTommITwThDmUr016iX5GAG";
+  const query = "&q=";
+  const limitOffsetRatingLang = "&limit=5&offset=0&rating=g&lang=en";
+  let fetchURL = `${baseURL}${apiKey}${query}${searchKey}${limitOffsetRatingLang}`;
 
-    console.log(`fetchURL=${fetchURL}`);
+  console.log(`fetchURL=${fetchURL}`);
 
-    let jsonResponse;
-    let url;
+  let jsonResponse;
+  let url;
 
-    return await fetch(fetchURL, { mode: "cors" });
+  return await fetch(fetchURL, { mode: "cors" })
     /*
     .then(function(response) {
         console.log("Response 1");
@@ -33,18 +78,23 @@ async function getGiph(searchKey) {
         console.log("An Error Occurred");
     });
     */
+    .then(function (response) {
+      console.log("Response 3");
+      console.log(response);
+      return response.json();
+    })
+    .then(function (response) {
+      console.log("Response 4");
+      console.log(response);
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(`Error=${error}`);
+    });
 }
 
-getGiph("rat")
-.then(function(response) {
-    console.log("Response 3");
-    console.log(response);
-    return response.json();
-})
-.then(function(response){
-    console.log("Response 4");
-    console.log(response.data[0].images.original.url);
-})
-.catch(function(error) {
-    console.log(`Error=${error}`);
-});
+function validateForm(searchKey) {
+  console.log("---Begin validateForm()---");
+  console.log(`Parameters=${searchKey}`);
+  return true;
+}
